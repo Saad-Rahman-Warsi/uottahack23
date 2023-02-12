ab091ed7fbe92660d9325b52a4fc262616f32b16
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-02-12 09:44:54 -0500

    proj

---
+++ solace-kotlin-android-paho-example/app/src/main/java/com/example/myapplication/MessagingOptions.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+
+// Solace PubSub+ Broker Options
+
+// Fill in your Solace Cloud PubSub+ Broker's 'MQTT Host' and 'Password' options.
+// This information can be found under:
+// https://console.solace.cloud/services/ -> <your-service> -> 'Connect' -> 'MQTT'
+const val SOLACE_CLIENT_USER_NAME = "solace-cloud-client"
+const val SOLACE_CLIENT_PASSWORD = "vq24ssb2ddl747djs4ptio61hr"
+const val SOLACE_MQTT_HOST = "ssl://mr-connection-du4vlnl351m.messaging.solace.cloud:8883"
+
+// Other options
+const val SOLACE_CONNECTION_TIMEOUT = 3
+const val SOLACE_CONNECTION_KEEP_ALIVE_INTERVAL = 60
+const val SOLACE_CONNECTION_CLEAN_SESSION = true
+const val SOLACE_CONNECTION_RECONNECT = true
